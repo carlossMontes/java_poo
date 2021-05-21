e55edfcab25d5d88461078ff7778bc95161c8fd3
chaozwoah <carloss.montes@gmail.com>
2021-05-20 23:37:02 -0500

    Menu mascota

---
+++ src/mascota/convivirMascota.java
@@ -0,0 +1,10 @@
+package mascota;
+
+public class convivirMascota {
+	public static void main(String[] args) {
+		System.out.println("Bienvenido al programa 'Convivir con tu mascota'\n");
+		System.out.println("Por favor selecciona una opción en el menú:");
+		System.out.println("1. Alimentar -->\t Aumenta el 10% de vida\n2. Golpear   -->\t Quita 10% de vida\n3. Revivir   -->\t Es obvio\n4. Matar     -->\t Es obvio\n5. Salir     -->\t Cerrar la aplicación");
+	}
+
+}
