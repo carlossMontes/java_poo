4df9dd8439cd139dc514a629ec18b0356dd7133e
chaozwoah <carloss.montes@gmail.com>
2021-05-27 23:38:48 -0500

    medio finish

--- src/matrices/TestMatrices.java
+++ src/matrices/TestMatrices.java
@@ -1,19 +1,29 @@
 package matrices;
 
+import java.util.Random;
+
 public class TestMatrices {
 
 	public static void main(String[] args) {
 
 		// 3 filas, 4 columnas
-		int[][] nNums = new int[10][10];
+		int[][] nNums = new int[3][3];
+
+		Random rd = new Random();
+
+		int cont = 0;
+		for (int i = 0; i < nNums.length; i++) { // Ciclo para las filas
+			for (int j = 0; j < nNums[i].length; j++) { // Ciclo para las columnas
+				nNums[i][j] = cont;
+				cont++;
+			}
+		}
 
 		for (int i = 0; i < nNums.length; i++) { // Ciclo para las filas
 			for (int j = 0; j < nNums[i].length; j++) { // Ciclo para las columnas
-				nNums[i][j] = 1;
-				System.out.println("[" + i + "]" + "[" + j + "]");
+				System.out.print(nNums[i][j] + " ");
 			}
-			
-			System.out.println("-------------------");
+			System.out.println();
 		}
 	}
 
