36bc069152b7ecbe1a4a1ad3277f54eae0df66b1
chaozwoah <carloss.montes@gmail.com>
2021-05-28 18:57:14 -0500

    lectura de archivo log para creacion de nuevos archivos

--- src/matrices/TestMatrices.java
+++ src/matrices/TestMatrices.java
@@ -1,6 +1,9 @@
 package matrices;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Random;
+import java.util.Scanner;
 
 public class TestMatrices {
 
@@ -8,8 +11,28 @@ public class TestMatrices {
 
 		// 3 filas, 4 columnas
 		int[][] nNums = new int[3][3];
+		String url = "/home/cmontes/Documentos/java/nums/";
+		int contador = 0;
 
+		// Objetos
 		Random rd = new Random();
+		File arch = new File(url + "log.txt");
+		Scanner sc = null;
+		
+		try {
+			System.out.println("...Leyendo el log");
+			sc = new Scanner(arch);
+			
+			while(sc.hasNextLine()) {
+				sc.nextLine();
+				contador = contador + 1;
+			}
+			
+			System.out.println("La cantidad de lineas es "+ contador);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		int cont = 0;
 		for (int i = 0; i < nNums.length; i++) { // Ciclo para las filas
