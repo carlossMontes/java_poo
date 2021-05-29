f48475226a65b5c6ccf1d243b11a2093c02de3b8
chaozwoah <carloss.montes@gmail.com>
2021-05-28 19:07:23 -0500

    lectura pero no preparada para linux

--- src/matrices/TestMatrices.java
+++ src/matrices/TestMatrices.java
@@ -2,12 +2,14 @@ package matrices;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class TestMatrices {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		// 3 filas, 4 columnas
 		int[][] nNums = new int[3][3];
@@ -16,21 +18,36 @@ public class TestMatrices {
 
 		// Objetos
 		Random rd = new Random();
-		File arch = new File(url + "log.txt");
+		File logR = new File(url + "log.txt");
+		FileWriter logW = new FileWriter(url + "log.txt");
+		FileWriter txt = null;
 		Scanner sc = null;
-		
+
+		// Lectura de log;
 		try {
 			System.out.println("...Leyendo el log");
-			sc = new Scanner(arch);
-			
-			while(sc.hasNextLine()) {
+			sc = new Scanner(logR);
+
+			while (sc.hasNextLine()) {
 				sc.nextLine();
-				contador = contador + 1;
+				contador++;
 			}
-			
-			System.out.println("La cantidad de lineas es "+ contador);
+
+			System.out.println("La cantidad de lineas es " + contador);
+
+			contador++;
+			logW.write(contador);
+			logW.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			txt = new FileWriter(url + "nums-" + contador);
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Existió algún error en el proceso");
 			e.printStackTrace();
 		}
 
