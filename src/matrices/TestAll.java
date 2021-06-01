ba00d9cf6167adde186a0f423ed8b24b1c08cde9
chaozwoah <carloss.montes@gmail.com>
2021-05-31 21:09:33 -0500

    Trabajando log, fecha y hora además de formato csv

--- src/matrices/TestAll.java
+++ src/matrices/TestAll.java
@@ -5,22 +5,31 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
 import java.util.Scanner;
 
 public class TestAll {
 
 	public static void main(String[] args) {
-		
+
+		// 3 filas, 4 columnas
+		int[][] nNums = new int[3][3];
+
 		// Declaracion de variables globales
 		String url = "/home/cmontes/Documentos/java/nums/";
 		int contador = 0;
 
 		// Objetos creados. Los nulos serán instanciados más adelante
-		File leerArchivo = new File(url + "log.txt");
+		File leerArchivo = new File(url + "info.log");
 		FileWriter crearArchivo = null;
 		Scanner sc = null;
 		BufferedWriter bw = null;
 		FileWriter fw = null;
+		Random rd = new Random();
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		LocalDateTime now = LocalDateTime.now();
 
 		// Lectura de log
 		try {
@@ -52,15 +61,30 @@ public class TestAll {
 		// Creación de nuevo archivo
 		try {
 
-			// Objetos instanciados. Se crea el escritor y el que toma todo lo que había antes
+			// Objetos instanciados. Se crea el escritor y el que toma todo lo que había
+			// antes
 			fw = new FileWriter(leerArchivo.getAbsoluteFile(), true);
 			bw = new BufferedWriter(fw);
+			bw.write(dtf.format(now) + " ");
 			bw.write(Integer.toString(contador + 1) + "\n");
 			System.out.println("información agregada!");
 
 			// Se crea nuevo archivo con el numero correspondiente del log
-			crearArchivo = new FileWriter(url + "nums-" + contador + ".txt");
-			crearArchivo.write("Construido");
+			crearArchivo = new FileWriter(url + "nums-" + contador + ".csv");
+
+			for (int i = 0; i < nNums.length; i++) { // Ciclo para las filas
+				for (int j = 0; j < nNums[i].length; j++) { // Ciclo para las columnas
+					nNums[i][j] = rd.nextInt(100);
+				}
+			}
+
+			for (int i = 0; i < nNums.length; i++) { // Ciclo para las filas
+				for (int j = 0; j < nNums[i].length; j++) { // Ciclo para las columnas
+					crearArchivo.write(nNums[i][j] + ",");
+				}
+				crearArchivo.write("\n");
+			}
+
 			crearArchivo.close();
 
 		} catch (Exception ex) {
@@ -74,8 +98,8 @@ public class TestAll {
 				if (fw != null)
 					fw.close();
 			} catch (IOException ex) {
-	            ex.printStackTrace();
-	        }
+				ex.printStackTrace();
+			}
 		}
 
 	}
--- src/matrices/TestMatrices.java
+++ src/matrices/TestMatrices.java
@@ -13,64 +13,22 @@ public class TestMatrices {
 
 		// 3 filas, 4 columnas
 		int[][] nNums = new int[3][3];
-		String url = "/home/cmontes/Documentos/java/nums/";
-		int contador = 0;
 
 		// Objetos
 		Random rd = new Random();
-		File fichero = new File(url + "log.txt");
-//		FileWriter logW = new FileWriter(url + "log.txt");
-		FileWriter txt = null;
-		Scanner s = null;
 
-		// Lectura de log;
-		// Fichero del que queremos leer
-
-		try {
-			// Leemos el contenido del fichero
-			System.out.println("... Leemos el contenido del fichero ...");
-			s = new Scanner(fichero);
-
-			// Leemos linea a linea el fichero
-			while (s.hasNextLine()) {
-				String linea = s.nextLine(); // Guardamos la linea en un String
-				System.out.println(linea); // Imprimimos la linea
-			}
-
-		} catch (Exception ex) {
-			System.out.println("Mensaje: " + ex.getMessage());
-		} finally {
-			// Cerramos el fichero tanto si la lectura ha sido correcta o no
-			try {
-				if (s != null)
-					s.close();
-			} catch (Exception ex2) {
-				System.out.println("Mensaje 2: " + ex2.getMessage());
+		for (int i = 0; i < nNums.length; i++) { // Ciclo para las filas
+			for (int j = 0; j < nNums[i].length; j++) { // Ciclo para las columnas
+				nNums[i][j] = rd.nextInt(100);
 			}
 		}
 
-//		try {
-//			txt = new FileWriter(url + "nums-" + contador);
-//		} catch (Exception e) {
-//			// TODO: handle exception
-//			System.out.println("Existió algún error en el proceso");
-//			e.printStackTrace();
-//		}
-
-//		int cont = 0;
-//		for (int i = 0; i < nNums.length; i++) { // Ciclo para las filas
-//			for (int j = 0; j < nNums[i].length; j++) { // Ciclo para las columnas
-//				nNums[i][j] = cont;
-//				cont++;
-//			}
-//		}
-//
-//		for (int i = 0; i < nNums.length; i++) { // Ciclo para las filas
-//			for (int j = 0; j < nNums[i].length; j++) { // Ciclo para las columnas
-//				System.out.print(nNums[i][j] + " ");
-//			}
-//			System.out.println();
-//		}
+		for (int i = 0; i < nNums.length; i++) { // Ciclo para las filas
+			for (int j = 0; j < nNums[i].length; j++) { // Ciclo para las columnas
+				System.out.print(nNums[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 
 }
